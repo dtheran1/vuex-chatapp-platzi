0edd4bcbcc52bef707ca63e7db11955f8bbe546a
Dtheran <dtheran1@gmail.com>
2023-01-11 11:19:55 -0500

    contacts

--- src/store/modules/messages/index.js
+++ src/store/modules/messages/index.js
@@ -7,6 +7,8 @@ const module = {
       messages: [
         { id: 1, author: 1, message: 'Hola 👀', timestamp: new Date().toLocaleTimeString(), read: false, channelId: 1 },
         { id: 2, author: 2, message: 'Hola desde el canal 2 👀', timestamp: new Date().toLocaleTimeString(), read: false, channelId: 2 },
+        { id: 3, author: 2, message: 'Hola tuu en el canal 2', timestamp: new Date().toLocaleTimeString(), read: false, channelId: 2 },
+        { id: 4, author: 1, message: 'Soy Daniel', timestamp: new Date().toLocaleTimeString(), read: false, channelId: 2 },
       ]
     }
   },
